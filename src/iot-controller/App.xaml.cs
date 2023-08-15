53feb68e0ef72e015a33625c10fe8b731eced4ac
yadian <yadianfdez07@gmail.com>
2023-08-14 20:05:58 -0400

    project re-created

---
+++ src/iot-controller/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using iot_controller.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace iot_controller
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ src/iot-controller/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace iot_controller.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
